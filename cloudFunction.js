38f725df56ac8ecfeac76bef630826d79d5aea11
Mitch Brogan <info@b33pb00p.art>
2023-07-01 12:39:44 -0400

    Initial commit

--- cloudFunction.js
+++ cloudFunction.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // MongoDB model
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongodb://localhost:27017/walletVerification, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const walletSchema = new Schema({
     address: String,
