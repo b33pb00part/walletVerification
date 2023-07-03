fdff302cd956614837f9bcbd3dda671f645e42db
Mitch Brogan <info@b33pb00p.art>
2023-07-03 02:44:41 -0400

    updated btcverify

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  entry: './public/walletConnections.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  target: 'web',
-  resolve: {
-    fallback: {
-      "http": false, 
-      "https": false, 
-      "crypto": false, 
-      "stream": false, 
-      "buffer": false
-    }
-  },
+    entry: './public/walletConnections.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    target: 'web',
+    resolve: {
+        fallback: {
+            "fs": false,
+            "tls": false,
+            "net": false,
+            "path": false,
+            "zlib": false,
+            "http": false,
+            "https": false,
+            "stream": false,
+            "crypto": false,
+        },
+    },
 };
+
