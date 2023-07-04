22e3f7e95a3dc257c880bb269e5a98e080fa2ee1
Mitch Brogan <info@b33pb00p.art>
2023-07-03 22:10:45 -0400

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    entry: './public/walletConnections.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    target: 'web',
-    resolve: {
-        fallback: {
-            "fs": false,
-            "tls": false,
-            "net": false,
-            "path": false,
-            "zlib": false,
-            "http": false,
-            "https": false,
-            "stream": false,
-            "crypto": false,
+  entry: {
+    app: './public/walletConnections.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'walletConnections.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-    },
+      },
+    ],
+  },
 };
-
